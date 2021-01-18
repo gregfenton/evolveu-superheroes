89451a231f505422709e259e35e822314426b818
Greg Fenton <greg.fenton@gmail.com>
2021-01-18 17:13:52 -0500

    FIX: detail GET API now a "skeleton" function Previous version had issues: (a) was using "/:name" - should be looking based on :id (b) was using a non-existent SUPERHEROES variable

--- server/routes/superhero.js
+++ server/routes/superhero.js
@@ -7,17 +7,21 @@ const Superhero = require('../models/Superhero');
 /* List all superheroes. */
 router.get('/', async (req, res, next) => {
   let data = await Superhero.find({});
-  console.info(`records retrieved from mongoose:`, data?.length)
+  console.info(`records retrieved from mongoose:`, data?.length);
   res.send(data);
 });
 
 /* Details of a given superhero. */
-router.get('/:name', function(req, res, next) {
-  let name = req.params.name;
-  if (! name) {
-    res.status(400).send('Bad request - `name` missing')
+router.get('/:id', function (req, res, next) {
+  let superheroId = req.params.id;
+  if (!superheroId) {
+    res.status(400).send('Bad request - `id` parameter missing');
   }
-  res.send(SUPERHEROES.filter(hero => hero.name === name));
+  // TODO: Replace this string with a query to Mongoose for value :id
+  let mySuperhero = `Looking for hero ${superheroId}?`;
+
+  // TODO: send the data in the response
+  res.send(mySuperhero);
 });
 
 module.exports = router;
